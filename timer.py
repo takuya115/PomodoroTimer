c464de531749889085e041f4571f90329358c5e9
u_taku115 <ut1995j15@gmail.com>
2019-06-15 16:42:33 +0900

    コマンドラインベースのタイマー完成

--- timer.py
+++ timer.py
@@ -5,9 +5,16 @@ import time
 #25分のタイマー
 def work():
     time.sleep(60*25)
+    print("work end")
     return relax()
 #5分休憩のタイマー
 def relax():
     time.sleep(60*5)
+    print("relax end")
     return work()
+
+if __name__ == "__main__":
+    print("starttimer")
+    work()
+    
 
